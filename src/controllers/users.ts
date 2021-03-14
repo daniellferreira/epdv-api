ce65edaf4d8063e5f6d7c30be2a1a6eb5bde42b9
Daniel Lopes Ferreira <daniel.aferreira@mercadolivre.com>
2021-03-14 16:08:45 -0300

    test

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -14,9 +14,11 @@ export class UserController {
   ): Promise<void> {
     try {
       const newUser = await this.service.create(req.body)
+      console.log('test')
       res.status(201).send(newUser)
     } catch (err) {
       next(err)
     }
   }
+
 }
