5160803f6adeb1ea5287c730ec3a0070028284b0
Maxwell F. Barbosa <maxwell.fbarbosa@gmail.com>
2021-05-27 20:06:05 -0300

    PDV=266 | Completed auth tests

--- test/functional/auth.test.ts
+++ test/functional/auth.test.ts
@@ -1,60 +1,84 @@
-import AuthService from '@src/lib/auth'
-import { authMiddleware } from '@src/middlewares/auth'
+import { Account } from '@src/models/account'
+import { User } from '@src/models/user'
 
-describe('AuthMiddleware', () => {
-  it('should verify a JWT token and call the next middleware', async () => {
-    const jwtToken = AuthService.generateToken({ data: 'fake' })
-    const reqFake = {
-      headers: {
-        'x-access-token': jwtToken['auth-token'],
-      },
+describe('Auth functional tests', () => {
+  let token: any
+  beforeEach(async () => {
+    await User.deleteMany()
+    await Account.deleteMany()
+    const newAccount = {
+      name: 'Account test',
+      email: 'AccountTest@mail.com',
     }
-    const resFake = {}
-    const nextFake = jest.fn()
 
-    authMiddleware(reqFake, resFake, nextFake)
-    expect(nextFake).toHaveBeenCalled()
+    await global.testRequest
+      .post('/accounts')
+      .send({ ...newAccount, password: 'test123456' })
+
+    const responseToken = await global.testRequest
+      .post('/auth/token')
+      .send({ email: newAccount.email, password: 'test123456' })
+    token = responseToken.body['auth-token']
   })
+  describe('when authenticating a user', () => {
+    it('should generate a token for a valid user', async () => {
+      const pwd = 'test123456'
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        tenantId: '1',
+      }
 
-  it('should return UNAUTHORIZED if there is a problem on the token verification', () => {
-    const reqFake = {
-      headers: {
-        'x-access-token': 'invalid token',
-      },
-    }
-    const sendMock = jest.fn()
-    const resFake = {
-      status: jest.fn(() => ({
-        send: sendMock,
-      })),
-    }
-    const nextFake = jest.fn()
-    authMiddleware(reqFake, resFake as object, nextFake)
-    expect(resFake.status).toHaveBeenCalledWith(401)
-    expect(sendMock).toHaveBeenCalledWith({
-      cause: 'UNAUTHORIZED',
-      message: 'jwt malformed',
+      await global.testRequest
+        .post('/users')
+        .set({
+          'x-access-token': token,
+        })
+        .send({ ...newUser, password: pwd })
+
+      const responseAuth = await global.testRequest
+        .post('/auth/token')
+        .send({ email: newUser.email, password: pwd })
+      expect(responseAuth.body).toEqual(
+        expect.objectContaining({ 'auth-token': expect.any(String) })
+      )
     })
-  })
+    it('Should not get user if email not found', async () => {
+      const myEmail = 'mail@mail.com'
+      const response = await global.testRequest
+        .post('/auth/token')
 
-  it('should return UNAUTHORIZED if there is no token', async () => {
-    const reqFake = {
-      headers: {},
-    }
-    const sendMock = jest.fn()
-    const resFake = {
-      status: jest.fn(() => ({
-        send: sendMock,
-      })),
-    }
-    const nextFake = jest.fn()
+        .send({ email: myEmail, password: '1234' })
 
-    authMiddleware(reqFake, resFake as object, nextFake)
+      expect(response.status).toBe(404)
+      expect(response.body).toEqual({
+        cause: 'RECORD_NOTFOUND',
+        message: `Record not found with email: ${myEmail}`,
+      })
+    })
 
-    expect(resFake.status).toHaveBeenCalledWith(401)
-    expect(sendMock).toHaveBeenCalledWith({
-      cause: 'UNAUTHORIZED',
-      message: 'jwt must be provided',
+    it('Should not get user if password does not match', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        tenantId: '1',
+      }
+      const responseCreate = await global.testRequest
+        .post('/users')
+        .set({
+          'x-access-token': token,
+        })
+        .send({ ...newUser, password: 'test123456' })
+
+      const response = await global.testRequest
+        .post('/auth/token')
+        .send({ email: newUser.email, password: 'different password' })
+
+      expect(response.status).toBe(401)
+      expect(response.body).toEqual({
+        cause: 'UNAUTHORIZED',
+        message: `Invalid password`,
+      })
     })
   })
 })
