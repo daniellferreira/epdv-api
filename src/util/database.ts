59b33c6c9b1326ced09ab45895ff84d8d8729f2a
Daniel Lopes Ferreira <daniel.aferreira@mercadolivre.com>
2021-06-01 22:42:56 -0300

    PDV-240 | fix mongo connect

--- src/util/database.ts
+++ src/util/database.ts
@@ -3,7 +3,7 @@ import mongoose, { Mongoose } from 'mongoose'
 
 export const connect = async (): Promise<Mongoose> => {
   mongoose.set('debug', process.env.NODE_ENV === 'development')
-  return await mongoose.connect(config.db.connectionString, {
+  return mongoose.connect(config.db.connectionString, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
