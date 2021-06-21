7c585dee8b8538dd4d8d006cf8efb39e2e5d289f
Maxwell F. Barbosa <maxwell.fbarbosa@gmail.com>
2021-06-21 11:59:55 -0300

    Change to unable disable all administrators

--- src/services/users.ts
+++ src/services/users.ts
@@ -71,11 +71,20 @@ export class UsersService {
 
   // eslint-disable-next-line
   public async edit(account: string, _id: string, data: any): Promise<User> {
-    if (data.active === false || data.active === 'false') {
-      const users_active = await User.find({ account, active: true })
+    if (
+      data.active === false ||
+      data.active === 'false' ||
+      data.isAdmin == false ||
+      data.isAdmin == 'false'
+    ) {
+      const users_active = await User.find({
+        account,
+        active: true,
+        isAdmin: true,
+      })
       if (users_active.length <= 1 && users_active[0]._id == _id) {
         throw new UserError(
-          `Unable to disable all users`,
+          `Unable to disable all administrators`,
           UserStatusCodes.Unauthorized,
           'UNAUTHORIZED'
         )
